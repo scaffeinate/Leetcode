3f4b53fac8691289fd2915d57ad1bfcb54040347
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2018-01-10 02:08:57 -0500

    Correct the silly error

--- ruby/easy/excel_sheet_column_number.rb
+++ ruby/easy/excel_sheet_column_number.rb
@@ -1,10 +1,12 @@
 # @param {String} s
 # @return {Integer}
 def title_to_number(s)
-    res, fact = 0, 26
-    s.each_char do |c|
-        res += (c.ord - 'A'.ord + 1) * fact
-        fact * 26
+    i, res, fact = s.length-1, 0, 1
+    while i >= 0
+        c = s[i]
+        res += ((c.ord - 'A'.ord + 1) * fact)
+        fact *= 26
+        i -= 1
     end
     return res
 end
