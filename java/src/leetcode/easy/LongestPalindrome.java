9acb165276ed17998036a286dc34fc47b848a059
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2018-01-14 17:20:09 -0500

    Added solutions for Leetcode 409. Longest Palindrome

---
+++ java/src/leetcode/easy/LongestPalindrome.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int longestPalindrome(String s) {
+        Set<Character> counter = new HashSet<Character>();
+        int count = 0;
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(counter.contains(c)) {
+                counter.remove(c);
+                count += 2;
+            } else {
+                counter.add(c);
+            }
+        }
+        
+        return count + ((count % 2 == 0) ? (counter.isEmpty() ? 0 : 1) : 0);
+    }
+}
