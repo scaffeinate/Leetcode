92afc255c3012200248ff3e26846b9ab2f9eaf48
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2018-01-17 02:47:21 -0500

    Fix

--- java/src/leetcode/easy/ArrayPartitionI.java
+++ java/src/leetcode/easy/ArrayPartitionI.java
@@ -1,6 +1,7 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
         int sum = 0;
+        Arrays.sort(nums);
         for(int i = 0; i < nums.length-1; i+=2) {
             sum += nums[i];
         }
